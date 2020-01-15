9ed4145bddaeffd79c08b91a0d0a25fb5981600b
bcarlso1 <bcarlso1@gmail.com>
2020-01-15 18:35:29 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
 document.getElementById("contact-link").addEventListener("click", (e) => {
     document.getElementById("contact-modal").style.display = "block";
-})
+});
 
 document.getElementById("close-modal").addEventListener("click", (e) => {
     document.getElementById("contact-modal").style.display = "none";
-})
+});
+
+
+
 
 document.getElementById("JS").addEventListener("mouseover", (e) => {
-    document.getElementById("JS").style.fontSize = "26px";
-    document.getElementById("JS").innerHTML = "Experience with Chart JS";
-})
+    if (window.innerWidth < 768) {
+        document.getElementById("JS").style.fontSize = "18px"; 
+    } else {
+        document.getElementById("JS").style.fontSize = "26px"; 
+    }
+document.getElementById("JS").innerHTML = "JS Syntax, DOM Manipulation, Fetch API/Ajax, JQuery, Local Storage";
+});
+
+        document.getElementById("JS").addEventListener("mouseout", (e) => {
+            document.getElementById("JS").style.fontSize = "48px";
+            document.getElementById("JS").innerHTML = "Javascript &#8250;&#8250; ";
+        });
+
 
 document.getElementById("basics").addEventListener("mouseover", (e) => {
-    document.getElementById("basics").style.fontSize = "26px";
-    document.getElementById("basics").innerHTML = "The Basics";
-})
+    if (window.innerWidth < 768) {
+        document.getElementById("basics").style.fontSize = "18px"; 
+    } else {
+        document.getElementById("basics").style.fontSize = "26px"; 
+    }
+    document.getElementById("basics").innerHTML = "Media Queries, CSS Styling, CSS Grid, Flex Box, Box Model, Display, Floats, Positioning, Semantic HTML, Bootstrap, SASS";
+});
+
+        document.getElementById("basics").addEventListener("mouseout", (e) => {
+            document.getElementById("basics").style.fontSize = "48px";
+            document.getElementById("basics").innerHTML = "HTML & CSS &#8250;&#8250; ";
+        });
+
+
 
 document.getElementById("more").addEventListener("mouseover", (e) => {
-    document.getElementById("more").style.fontSize = "26px";
-    document.getElementById("more").innerHTML = "Github, SASS";
-})
+    if (window.innerWidth < 768) {
+        document.getElementById("more").style.fontSize = "18px"; 
+    } else {
+        document.getElementById("more").style.fontSize = "26px"; 
+    }
+    document.getElementById("more").innerHTML = "Terminal/Git Basics, Github, Peer Reviews";
+});
 
-document.getElementById("JS").addEventListener("mouseout", (e) => {
-    document.getElementById("JS").style.fontSize = "48px";
-    document.getElementById("JS").innerHTML = "Javascript &#8250;&#8250; ";
-})
-
-document.getElementById("basics").addEventListener("mouseout", (e) => {
-    document.getElementById("basics").style.fontSize = "48px";
-    document.getElementById("basics").innerHTML = "HTML & CSS &#8250;&#8250; ";
-})
-
-document.getElementById("more").addEventListener("mouseout", (e) => {
-    document.getElementById("more").style.fontSize = "48px";
-    document.getElementById("more").innerHTML = "More &#8250;&#8250;";
-})
+        document.getElementById("more").addEventListener("mouseout", (e) => {
+            document.getElementById("more").style.fontSize = "48px";
+            document.getElementById("more").innerHTML = "More &#8250;&#8250;";
+        });
